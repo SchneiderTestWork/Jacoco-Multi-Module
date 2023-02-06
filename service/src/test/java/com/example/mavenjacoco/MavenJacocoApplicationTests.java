8b213ba4afe04a5df394f7e9cd12bcc9271e17fa
Vladimir Schneider <schneider_v_s@mail.ru>
2023-02-06 11:03:25 +0300

    Add Project

---
+++ service/src/test/java/com/example/mavenjacoco/MavenJacocoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mavenjacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJacocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        MavenJacocoApplication.main(new String[] {"one"});
+    }
+
+}
